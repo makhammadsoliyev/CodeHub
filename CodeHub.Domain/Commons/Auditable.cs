56fe430da83b6d973ec6b1b5986145aeffdd5e78
Javohir <xursanboyevj26@gmail.com>
2024-03-20 15:28:27 +0500

    Added Auditable

---
+++ CodeHub.Domain/Commons/Auditable.cs
@@ -0,0 +1,8 @@
+﻿namespace CodeHub.Domain.Commons;
+
+public class Auditable
+{
+    public long Id { get; set; }    
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set;}
+}
