bfda96eeab41b84608c759d94db7bb69aeabd88e
Javohir <xursanboyevj26@gmail.com>
2024-03-20 15:29:18 +0500

    Created Auditable

--- CodeHub.Domain/Commons/Auditable.cs
+++ CodeHub.Domain/Commons/Auditable.cs
@@ -3,6 +3,6 @@
 public class Auditable
 {
     public long Id { get; set; }    
-    public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set;}
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set;}
 }
