236ac11fa7649939747f044935581372eb5c6b39
Umidbek Maxammadsoliyev <149594973+makhammadsoliyev@users.noreply.github.com>
2024-03-21 17:35:10 +0500

    Update RepositoryViewModel.cs

--- CodeHub.Model/Repositories/RepositoryViewModel.cs
+++ CodeHub.Model/Repositories/RepositoryViewModel.cs
@@ -5,11 +5,11 @@ namespace CodeHub.Model.Repositories;
 
 public class RepositoryViewModel
 {
-    public long ID { get; set; }
+    public long Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public string BranchName { get; set; }
-    public long UserID { get; set; }
+    public long UserId { get; set; }
     public RepositoryVisibility Visibility { get; set; }
     public Readme Readme { get; set; }
     public GitIgnore GitIgnore { get; set; }
