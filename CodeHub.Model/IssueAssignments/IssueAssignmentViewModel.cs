f4c7c6a03fff19dcd75cf44c9d1782b03d8e75c1
Umidbek Maxammadsoliyev <149594973+makhammadsoliyev@users.noreply.github.com>
2024-03-21 17:32:33 +0500

    Update IssueAssignmentViewModel.cs

--- CodeHub.Model/IssueAssignments/IssueAssignmentViewModel.cs
+++ CodeHub.Model/IssueAssignments/IssueAssignmentViewModel.cs
@@ -5,5 +5,6 @@ namespace CodeHub.Model.IssueAssignments;
 public class IssueAssignmentViewModel
 {
     public long Id { get; set; }
+    public Issue Issue { get; set; }
     public User Assigness { get; set; }
 }
