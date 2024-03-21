bb5e916a81f33d3ad07fbfbdc895bf6b967d4efc
Umidbek Maxammadsoliyev <149594973+makhammadsoliyev@users.noreply.github.com>
2024-03-21 17:31:25 +0500

    Update BranchRepositoryUpdateModel.cs

--- CodeHub.Model/BranchRepositories/BranchRepositoryUpdateModel.cs
+++ CodeHub.Model/BranchRepositories/BranchRepositoryUpdateModel.cs
@@ -5,5 +5,5 @@ public class BranchRepositoryUpdateModel
     public string BranchName { get; set; }
     public long? ReadmeId { get; set; }
     public long? GitIgnoreId { get; set; }
-    public long? LicenceId { get; set; }
+    public long? LicenseId { get; set; }
 }
