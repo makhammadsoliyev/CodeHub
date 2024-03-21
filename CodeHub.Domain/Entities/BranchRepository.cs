18a30cb6d6f61808f441d1ebd0d0e046a85e6d46
Umidbek Maxammadsoliyev <149594973+makhammadsoliyev@users.noreply.github.com>
2024-03-21 17:29:18 +0500

    Update BranchRepository.cs

--- CodeHub.Domain/Entities/BranchRepository.cs
+++ CodeHub.Domain/Entities/BranchRepository.cs
@@ -5,7 +5,7 @@ namespace CodeHub.Domain.Entities;
 public class BranchRepository : Auditable
 {
     public string BranchName { get; set; }
-    public long UserID { get; set; }
+    public long UserId { get; set; }
     public User User { get; set; }
     public long? ReadmeId { get; set; }
     public long? GitIgnoreId { get; set; }
