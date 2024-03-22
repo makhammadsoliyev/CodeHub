a20fbf7739f353b9627aed73f5f6537ba54cea85
Ozodbek <ozodbeknormamatov.2004@gmail.com>
2024-03-22 14:21:47 +0500

    Models and Entities

---
+++ CodeHub.Domain/Entities/License.cs
@@ -0,0 +1,9 @@
+﻿using CodeHub.Domain.Commons;
+
+namespace CodeHub.Domain.Entities;
+
+public class License : Auditable
+{
+    public string Name { get; set; }
+    public string Content { get; set; } 
+}
