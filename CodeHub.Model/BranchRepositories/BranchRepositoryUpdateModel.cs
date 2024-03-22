c28fb6e211bfcd588325c849e265d7b7ae8ec635
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-22 18:51:59 +0500

    Wrote BranchRepositoryService

--- CodeHub.Model/BranchRepositories/BranchRepositoryUpdateModel.cs
+++ CodeHub.Model/BranchRepositories/BranchRepositoryUpdateModel.cs
@@ -2,7 +2,10 @@
 
 public class BranchRepositoryUpdateModel
 {
+    public long Id { get; set; }
     public string BranchName { get; set; }
+    public long UserId { get; set; }
+    public long RepositoryId { get; set; }
     public long? ReadmeId { get; set; }
     public long? GitIgnoreId { get; set; }
     public long? LicenseId { get; set; }
