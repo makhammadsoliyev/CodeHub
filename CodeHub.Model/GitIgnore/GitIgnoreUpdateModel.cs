8e4f1904b5602ca0f6fe93ba40fcdbdae1080ac7
Umidbek Maxammadsoliyev <149594973+makhammadsoliyev@users.noreply.github.com>
2024-03-22 22:10:31 +0500

    Update GitIgnoreUpdateModel.cs

--- CodeHub.Model/GitIgnore/GitIgnoreUpdateModel.cs
+++ CodeHub.Model/GitIgnore/GitIgnoreUpdateModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CodeHub.Model.GitIgnore;
+namespace CodeHub.Model.GitIgnores;
 public class GitIgnoreUpdateModel
 {
     public long Id { get; set; }
