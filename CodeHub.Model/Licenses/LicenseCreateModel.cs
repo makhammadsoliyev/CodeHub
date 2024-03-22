947cd3985487d64045fb4431b922b70b60a82136
Umidjon <makhammadsoliyev@gmail.com>
2024-03-22 23:34:49 +0500

    Finish Mapper

--- CodeHub.Model/Licenses/LicenseCreateModel.cs
+++ CodeHub.Model/Licenses/LicenseCreateModel.cs
@@ -1,6 +1,6 @@
 ﻿namespace CodeHub.Model.Licenses;
 
-public class LicenseCreateModell
+public class LicenseCreateModel
 {
     public string Name { get; set; }
     public string Content { get; set; }
--- CodeHub.Service/Mappers/MappingProfile.cs
+++ CodeHub.Service/Mappers/MappingProfile.cs
@@ -1,10 +1,20 @@
 ﻿using AutoMapper;
 using CodeHub.Domain.Entities;
 using CodeHub.Model.BranchRepositories;
+using CodeHub.Model.Commits;
+using CodeHub.Model.File;
+using CodeHub.Model.Follow;
+using CodeHub.Model.GitIgnore;
 using CodeHub.Model.IssueAssignments;
 using CodeHub.Model.Issues;
+using CodeHub.Model.Licenses;
+using CodeHub.Model.Models.Folders;
+using CodeHub.Model.Readme;
 using CodeHub.Model.Repositories;
+using CodeHub.Model.RepositoryStar;
+using CodeHub.Model.RepositroyForks;
 using CodeHub.Model.Users;
+using File = CodeHub.Domain.Entities.File;
 
 namespace CodeHub.Service.Mappers;
 
@@ -31,5 +41,41 @@ public class MappingProfile : Profile
         CreateMap<BranchRepository, BranchRepositoryViewModel>().ReverseMap();
         CreateMap<BranchRepository, BranchRepositoryUpdateModel>().ReverseMap();
         CreateMap<BranchRepository, BranchRepositoryCreateModel>().ReverseMap();
+
+        CreateMap<File, FileViewModel>().ReverseMap();
+        CreateMap<File, FileUpdateModel>().ReverseMap();
+        CreateMap<File, FileCreateModel>().ReverseMap();
+
+        CreateMap<Folder, FolderViewModel>().ReverseMap();
+        CreateMap<Folder, FolderCreateModel>().ReverseMap();
+        CreateMap<Folder, FolderCreateModel>().ReverseMap();
+
+        CreateMap<RepositoryFork, RepositoryForkViewModel>().ReverseMap();
+        CreateMap<RepositoryFork, RepositoryForkUpdateModel>().ReverseMap();
+        CreateMap<RepositoryFork, RepositoryForkCreateModel>().ReverseMap();
+
+        CreateMap<Commit, CommitViewModel>().ReverseMap();
+        CreateMap<Commit, CommitUpdateModel>().ReverseMap();
+        CreateMap<Commit, CommitCreateModel>().ReverseMap();
+
+        CreateMap<GitIgnore, GitIgnoreViewModel>().ReverseMap();
+        CreateMap<GitIgnore, GitIgnoreUpdateModel>().ReverseMap();
+        CreateMap<GitIgnore, GitIgnoreCreateModel>().ReverseMap();
+
+        CreateMap<License, LicenseViewModel>().ReverseMap();
+        CreateMap<License, LicenseUpdateModel>().ReverseMap();
+        CreateMap<License, LicenseCreateModel>().ReverseMap();
+
+        CreateMap<Readme, ReadmeViewModel>().ReverseMap();
+        CreateMap<Readme, ReadmeUpdateModel>().ReverseMap();
+        CreateMap<Readme, ReadmeCreateModel>().ReverseMap();
+
+        CreateMap<RepositoryStar, RepositoryStarViewModel>().ReverseMap();
+        CreateMap<RepositoryStar, RepositoryStarUpdateModel>().ReverseMap();
+        CreateMap<RepositoryStar, RepositoryStarCreateModel>().ReverseMap();
+
+        CreateMap<Follow, FollowViewModel>().ReverseMap().ReverseMap();
+        CreateMap<Follow, FollowUpdateModel>().ReverseMap().ReverseMap();
+        CreateMap<Follow, FollowCreateModel>().ReverseMap().ReverseMap();
     }
 }
