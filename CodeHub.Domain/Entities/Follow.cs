c9681bcf031b7a9b85c32b1ca9248690d03d4a1e
Javohir <161417608+Javohir-Khursanboyev@users.noreply.github.com>
2024-03-22 14:00:26 +0500

    Update Follow.cs

--- CodeHub.Domain/Entities/Follow.cs
+++ CodeHub.Domain/Entities/Follow.cs
@@ -6,4 +6,5 @@ public class Follow : Auditable
     public long FollowerId {  get; set; }
     public User Follower { get; set; }
     public long FollowingId {  get; set; }
+    public User Follower { get; set; }
 }
