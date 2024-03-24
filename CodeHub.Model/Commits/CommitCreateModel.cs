181eb7eb843b8e4855011a8f5c0b855b967c9294
Mirjalol Ziyadullayev <112009351+ziyadullayevmirjalol@users.noreply.github.com>
2024-03-24 18:16:39 +0500

    Wrote CommitService and RepositoryForkService

--- CodeHub.Model/Commits/CommitCreateModel.cs
+++ CodeHub.Model/Commits/CommitCreateModel.cs
@@ -3,7 +3,7 @@
 public class CommitCreateModel
 {
     public long UserId { get; set; }
-    public long? RepositoryId { get; set; }
+    public long RepositoryId { get; set; }
     public long? BranchRepositoryId { get; set; }
     public string Message { get; set; }
 }
